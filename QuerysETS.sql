eb3b4e003febfc718ceb163c03d2d402707b5fed
Cesat089 <cesare.cabelloa@gmail.com>
2022-12-23 23:32:36 +0000

    PrimerasCuatroConsultas

---
+++ QuerysETS.sql
@@ -0,0 +1,49 @@
+use steam
+go
+
+--Lista todas las columnas de la tabla.
+
+select Nombre,ReleaseDate,RequiredAge,DemoCount,DeveloperCount,DLCCount,Metacritic,
+PublisherCount,AchievementCount,ControllerSupport,IsFree,FreeVerAvail,PlatformWindows,
+PlatformLinux,PlatformMac,Genre,PriceCurrency,PriceInitial,PriceFinal,ShortDescrip,
+HeaderImage,SupportedLanguages,Category,Website
+from juegos;
+go
+
+--Lista todos los diferentes géneros, eliminando duplicados.
+
+select distinct Genre
+from juegos
+where Genre != ''
+group by Genre
+order by Genre;
+go
+
+--Lista únicamente aquellos juegos que puedan ser jugados únicamente en Linux.
+
+select Nombre,PlatformLinux,PlatformMac,PlatformWindows
+from juegos
+where PlatformLinux = 'True' and PlatformWindows = 'False' and PlatformMac = 'False';
+go
+
+--Lista todos los juegos de rol, ordenador por nota.
+
+select Nombre,Genre,Metacritic
+from juegos
+where Genre like '%RPG%'
+order by Metacritic desc;
+go
+
+--Lista todos aquellos juegos que puedan ser jugados en Español pero no en Frances.
+
+
+
+--Lista el nombre del juego, su precio inicial, su precio final y la diferencia entre ambos en una nueva columna.
+
+
+
+--¿Cuántos juegos hay del siglo pasado?
+
+
+
+--Devuelve todos los juegos que terminen o en N o en O.
